5beebe9fed96fff33dc20c3f291829fd737417a0
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2016-07-12 16:47:26 -0400

    css changes

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
 console.log('ice')
-$(document).ready(function(){
-  $('.fa-chevron-up').click(function () {
-        $('body').animate({
-            scrollTop: 0
-        }, 400);
-        return false;
-    });
-});
